f8c11808a5baee081d38fd092dc238af4fd2dc98
unknown <five@DESKTOP-NL2NTAS.cdsafi.com>
2018-07-27 15:05:50 +0800

    swagger 加入讀取註解功能

--- app/Startup.cs
+++ app/Startup.cs
@@ -12,6 +12,8 @@ using Swashbuckle.AspNetCore.Swagger;
 using APISample.Filters;
 using APISample.Middleware;
 using APISample.Helper;
+using System.Reflection;
+using System.IO;
 
 
 namespace APISample
@@ -63,7 +65,16 @@ namespace APISample
                         Email = string.Empty
                     }
                 });
+
+                //設定註解路徑給Swagger JSON and UI.
+                
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
             });  
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
